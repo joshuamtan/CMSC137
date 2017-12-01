aaf055e4b17e727a36049d4d496b9fbd3477300a
charlesjom <cumarquez@up.edu.ph>
2017-12-02 00:43:50 +0800

    Added more constants to Constants interface

--- src/Constants.java
+++ src/Constants.java
@@ -1,4 +1,25 @@
 public interface Constants {
+    final String GAME_NAME = "Python Master Race";
+
     final int CHAT_PORT = 9999;
     final int GAME_PORT = 10000;
+    final int GAME_CLIENT_PORT = 8000;
+
+    final int GAME_WIDTH = 480;
+    final int CHAT_WIDTH = 320;
+    final int WINDOW_WIDTH = 800;
+    final int WINDOW_HEIGHT = 640;
+
+    // State identifiers
+    final int MENU_STATE = 0;   //menu state identifier
+    final int PLAY_STATE = 1;   //play state identifier
+    final int GAME_OVER_STATE = 2;   //game over state identifier
+    final int LOBBY_STATE = 3;   // lobby state identifier
+    final int WAITING_STATE = 4; // waiting state identifier
+
+    // Game stages
+    final int GAME_START = 0;
+    final int IN_PROGRESS = 1;
+    final int GAME_END = 2;
+    final int WAITING_FOR_PLAYERS = 3;
 }
